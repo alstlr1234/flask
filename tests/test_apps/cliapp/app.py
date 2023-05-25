a94ae8570f166ae8809bd542217b4fb871e88474
aaa <ksh23705@naver.com>
2023-05-25 21:59:50 +0900

    classify code_review, code_analyze

--- tests/test_apps/cliapp/app.py
+++ tests/test_apps/cliapp/app.py
@@ -4,16 +4,35 @@ import openai
 app = Flask(__name__)
 openai.api_key = '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'
 
-messages = []
-while True:
-    content = input("User:")
-    messages.append({"role":"user","content":content})
+def code_review():
+    messages = []
+    while True:
+        content = input("User:")
+        messages.append({"role":"user","content":content})
 
-    completion = openai.ChatCompletion.create(
+        completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
-    )
+        )
 
-    chat_response = completion.choices[0].message.content
-    print(f'ChatGPT: {chat_response}')
-    messages.append({"role":"assistant", "content":chat_response})
+        chat_response = completion.choices[0].message.content
+        print(f'ChatGPT: {chat_response}')
+        messages.append({"role":"assistant", "content":chat_response})
+        if content == "exit":
+            return 0
+def code_analyze():
+    messages = []
+    while True:
+        content = input("User:")
+        messages.append({"role":"user","content":content})
+
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+        )
+
+        chat_response = completion.choices[0].message.content
+        print(f'ChatGPT: {chat_response}')
+        messages.append({"role":"assistant", "content":chat_response})
+        if content == "exit":
+            return 0
