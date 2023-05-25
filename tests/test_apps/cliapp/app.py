c16f78304350568f05f4554bec879dc3b96c46e7
aaa <ksh23705@naver.com>
2023-05-25 17:34:35 +0900

    write openai,

--- tests/test_apps/cliapp/app.py
+++ tests/test_apps/cliapp/app.py
@@ -1,8 +1,30 @@
-# save this as app.py
-from flask import Flask
+from flask import Flask, request
+import openai
 
 app = Flask(__name__)
+openai.api_key = 'YOUR_API_KEY'
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+def process_code(code):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=code,
+        max_tokens=150
+    )
+    return response['choices'][0]['text']
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = ''
+    if request.method == 'POST':
+        code = request.form['code']
+        result = process_code(code)
+    return f'''
+        <form method="POST">
+            <textarea name="code"></textarea>
+            <input type="submit" value="Submit">
+        </form>
+        <div>{result}</div>
+    '''
+
+if __name__ == '__main__':
+    app.run()
