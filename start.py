971be68b5bd87e9acc70a30b10c454e6f8c257d3
mk2986 <cmk304504@naver.com>
2023-05-26 12:54:31 +0900

    edit main.py

---
+++ start.py
@@ -0,0 +1,39 @@
+from flask import Flask, send_from_directory, render_template
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/")
+def index():
+    return send_from_directory('templates',"index.html")
+
+@app.route('/<path:name>')
+def start(name):
+    return send_from_directory('templates',name)
+
+@app.route("/post")
+def index2():
+    return send_from_directory('templates',"index2.html")
+
+@app.route("/post")
+def load_index2():
+    return render_template("index2.html")
+
+@app.route("/post2")
+def index3():
+    return send_from_directory('templates',"index3.html")
+
+@app.route("/post2")
+def load_index3():
+    return render_template("index3.html")
+
+@app.route("/post3")
+def index4():
+    return send_from_directory('templates',"index4.html")
+
+@app.route("/post3")
+def load_index4():
+    return render_template("index4.html")
