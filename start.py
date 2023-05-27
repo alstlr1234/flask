b55f1ee5cad48a8f3b5edf23897831227025dfcc
mk2986 <cmk304504@naver.com>
2023-05-27 18:19:27 +0900

    homepage edit

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
-from flask import Flask, send_from_directory, render_template
-
+from flask import Flask, send_from_directory, render_template, request
+import openai
 app = Flask(__name__)
 
 @app.route("/hello")
@@ -37,3 +37,28 @@ def index4():
 @app.route("/post3")
 def load_index4():
     return render_template("index4.html")
+
+
+openai.api_key = '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'
+
+def code_review(content):
+    messages = [{"role":"user","content":content}]
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    chat_response = completion.choices[0].message.content
+    return chat_response
+
+@app.route("/post", methods=['GET', 'POST'])
+def review():
+    if request.method == 'POST':
+        content = request.form['content']
+        response = code_review(content)
+        return response
+
+
+
+
+
+
--- tests/test_apps/cliapp/app.py
+++ tests/test_apps/cliapp/app.py
@@ -30,7 +30,7 @@ def review():
                 async function submitForm(event) {
                     event.preventDefault();
                     const content = document.querySelector('#content').value;
-                    const response = await fetch('/', {
+                    const response = await fetch('http://127.0.0.1:5000/post', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded'
